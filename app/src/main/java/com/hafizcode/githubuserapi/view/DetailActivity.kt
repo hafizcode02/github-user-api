1c99775aa3e51841b2c5e894e4eb3a459eec768c
HafiCode02 <hafizcode02@gmail.com>
2020-06-18 17:24:34 +0700

    adding detail activity

---
+++ app/src/main/java/com/hafizcode/githubuserapi/view/DetailActivity.kt
@@ -0,0 +1,13 @@
+package com.hafizcode.githubuserapi.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hafizcode.githubuserapi.R
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
